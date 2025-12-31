31351e64d21e360c7fda1609c06b4824cf25d061
Maxwell Newberry <maxwellnewberry96@gmail.com>
2025-12-31 16:10:58 -0600

    Initial Commit

---
+++ SlurpSocial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlurpSocial
+//
+//  Created by Cali Shaw on 12/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
